42501921be837cc2a27c4fbdda49de8aeff09184
CrockettScience <johncrockett15@gmail.com>
2019-12-11 01:06:32 -0800

    Set up new test

--- Test/Program.cs
+++ Test/Program.cs
@@ -9,27 +9,27 @@ namespace Test
             var universe = multiverse.BaseUniverse;
             
             var galaxy = new Galaxy(multiverse);
-            var star = new Star(galaxy, multiverse);
-            
-            var rootStarContinuity = universe.GetContinuity(star);
-            
-            //Test branching multiverse
-            rootStarContinuity.Constrain(10_000_000_000, Star.Proto, out var d0);
-            rootStarContinuity.Constrain(11_000_000_000, Star.MainSequence, out d0);
-            rootStarContinuity.Constrain(11_000_000_000, Star.Massive, out var massiveStarDiff);
-            
-            var massiveStarUniverse = new Universe(massiveStarDiff);
-            var massiveStarContinuity = massiveStarUniverse.GetContinuity(star);
+            for (var i = 0; i < 1000; i++)
+            {
+                var star = new Star(galaxy, multiverse);
 
-            massiveStarContinuity.Constrain(13_000_000_351, Star.BlackHole, out d0);
-            massiveStarContinuity.Constrain(13_000_000_351, Star.Neutron, out var neutronStarDiff);
-            
-            var neutronStarUniverse = new Universe(neutronStarDiff);
-            var neutronStarContinuity = neutronStarUniverse.GetContinuity(star);
+                var rootStarContinuity = universe.GetContinuity(star);
 
-            var testPosition = rootStarContinuity[ulong.MaxValue];   //White Dwarf
-            testPosition = massiveStarContinuity[ulong.MaxValue];    //Black Hole
-            testPosition = neutronStarContinuity[ulong.MaxValue];    //Neutron Star
+                //Test branching multiverse
+                rootStarContinuity.Constrain(10_000_000_000, Star.Proto, out var d0);
+                rootStarContinuity.Constrain(11_000_000_000, Star.MainSequence, out d0);
+                rootStarContinuity.Constrain(11_000_000_000, Star.Massive, out var massiveStarDiff);
+
+                var massiveStarUniverse = new Universe(massiveStarDiff);
+                var massiveStarContinuity = massiveStarUniverse.GetContinuity(star);
+
+                massiveStarContinuity.Constrain(13_000_000_351, Star.BlackHole, out d0);
+                massiveStarContinuity.Constrain(13_000_000_351, Star.Neutron, out var neutronStarDiff);
+
+                var neutronStarUniverse = new Universe(neutronStarDiff);
+            }
+
+            var a = 10;
         }
     }
 }
